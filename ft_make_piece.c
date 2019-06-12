6b98aec950a4aae6a52a4065f25b18482234ae8e
Marina Couto <marinamacc@hotmail.com>
2019-06-11 17:07:15 -0700

    ft_make_piece trying to create the linked-list

--- ft_make_piece.c
+++ ft_make_piece.c
@@ -6,17 +6,34 @@
 /*   By: mcouto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 22:40:55 by mcouto            #+#    #+#             */
-/*   Updated: 2019/06/10 00:29:30 by mcouto           ###   ########.fr       */
+/*   Updated: 2019/06/11 17:06:32 by mcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- int main_make_piece(int width, int height, int letter)
- {
+t_list *make_list(t_piece tetramino)
+{
+	
+}
+
+t_piece *make_struct(int width, int height, int letter, char *tetramino)
+{
 		t_piece *piece;
 
  		piece = ft_memalloc(sizeof(t_piece));
  		piece->width = width;
  		piece->height = height;
- 		piece->next = next;
- 		tetri_letter = letter;
-  }
+ 		piece->tetri_letter = letter;
+		piece->next = NULL;
+
+		return (piece);
+}
+
+int make_piece_main (int width, int height, int letter, char *tetramino)
+{
+	if (letter == 'A')
+	{
+		
+	}
+}
+
+
